a9b9862be59521d75182563e483cf9d2bd42ccff
Alexander Sharapov <alexander@sharapov.biz>
2018-03-22 00:28:07 +0600

    adding concat example, removing forgotten printing text

--- examples/concatenate.php
+++ examples/concatenate.php
@@ -23,12 +23,50 @@ $ffmpeg = \Sharapov\FFMpegExtensions\FFMpeg::create( [
                                                        // The number of threads that FFMpeg should use
                                                      ] );
 
+$clip1 = $ffmpeg->open( new \Sharapov\FFMpegExtensions\Input\File( getcwd() . '/examples/source/demo_video_720p_HD.mp4' ) );
+
+// Create complex filter collection
+$options = new \Sharapov\FFMpegExtensions\Filters\Video\FilterComplexOptions\OptionsCollection();
+
+// Create drawtext option 1
+$text1 = new \Sharapov\FFMpegExtensions\Filters\Video\FilterComplexOptions\OptionDrawText();
+$text1
+  // Set z-index property. Greater value is always in front
+  ->setZIndex(160)
+  // You can use fade-in and fade-out effects. Set time in seconds
+  ->setFadeIn(2)
+  ->setFadeOut(2)
+  // Set font path
+  ->setFontFile(new \Sharapov\FFMpegExtensions\Input\File(getcwd() . '/examples/source/calibri.ttf'))
+  // Set font color. Accepts transparency value as the second argument. Float value between 0 and 1.
+  ->setFontColor('#ffffff')
+  // Set font size in pixels
+  ->setFontSize(33)
+  // Set text string
+  ->setText('php-ffmpeg-extensions>library')
+  // Coordinates where the text should be rendered. Accepts positive integer or
+  // constants "(w-tw)/2", "(h-th)/2" to handle auto-horizontal, auto-vertical values
+  ->setCoordinates(new \Sharapov\FFMpegExtensions\Coordinate\Point(\Sharapov\FFMpegExtensions\Coordinate\Point::AUTO_HORIZONTAL, 50))
+  // Set timings (start, stop) in seconds. Accepts float values as well
+  ->setTimeLine(new \Sharapov\FFMpegExtensions\Coordinate\TimeLine(1, 20));
+
+// Pass option to the options collection
+$options
+  ->add($text1);
+
+// Apply filter options to video
+$clip1
+  ->filters()
+  ->complex($options);
+
 $collection = new \Sharapov\FFMpegExtensions\Media\VideoCollection();
 
+// Adding clips to the collection
 $collection
-  ->add( $ffmpeg->open( new \Sharapov\FFMpegExtensions\Input\File( getcwd() . '/examples/source/demo_video_720p_HD.mp4' ) ) )
+  ->add( $clip1 )
   ->add( $ffmpeg->open( new \Sharapov\FFMpegExtensions\Input\File( getcwd() . '/examples/source/intro_720p_muted.mp4' ) ) );
 
+// And do concatenation
 $format = new \Sharapov\FFMpegExtensions\Format\Video\X264();
 $format->on( 'progress', function ( $item, $format, $percentage ) {
   print 'Done: ' . $percentage . "%\n";
